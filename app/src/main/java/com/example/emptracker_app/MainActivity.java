4959bdd22a72824d1266dbd10e6c897ad9193969
Chintan Suthar <chintan.suthar777@gmail.com>
2019-05-27 15:04:47 +1000

    Initial commit

---
+++ app/src/main/java/com/example/emptracker_app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.emptracker_app;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellayout1, rellayout2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellayout1.setVisibility(View.VISIBLE);
+            rellayout2.setVisibility(View.VISIBLE);
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rellayout1 = (RelativeLayout) findViewById(R.id.rellayout1);
+        rellayout2 = (RelativeLayout) findViewById(R.id.rellayout2);
+
+        handler.postDelayed(runnable, 3000); // timeout for splash screen
+    }
+}
